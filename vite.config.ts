a220ac9d4f0b9a4afa9d0ea7df3b9c642dd3be24
innowave620 <innowave620@gmail.com>
2025-04-07 15:36:22 +0200

    added a base repo directory

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/AI_Powered-portfolio",
   server: {
     host: true,
     allowedHosts: ['0438-41-116-75-31.ngrok-free.app'],
